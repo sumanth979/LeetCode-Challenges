ce0e0b9068116db70d52b541300dfc1c08697e75
sumanth <kanagala.sumanth@gmail.com>
2020-07-09 10:22:02 +0530

    Hamming Distance

---
+++ HammingDistance.java
@@ -0,0 +1,34 @@
+/*
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+*/
+
+class Solution {
+       public int hammingDistance(int x, int y) {
+           int res = x ^ y;
+           int setBits=0;
+           while(res>0){
+               setBits += res & 1;
+               // Bitwise right shift operator
+               res >>= 1;
+           }
+           return setBits;
+       }
+   }
